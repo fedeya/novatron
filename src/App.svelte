3da10598c18ec2a2d1c1d7c11f394bccd504dcff
SirReiva <gjjavicoru@hotmail.com>
2020-07-29 00:57:29 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import TextField from 'smelte/src/components/TextField';
+</script>
+
+<style>
+
+</style>
+
+<div class="container mx-auto">
+    <TextField label="Url" />
+</div>
