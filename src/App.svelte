58bc384d7f98b383dc35fb30443f8c32e8a88620
Fedeya <elfedeminaya@gmail.com>
2020-07-29 18:59:21 -0300

    fix: removed github styles for svelte highlight

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
-  import { github } from 'svelte-highlight/styles';
   import Request from './components/Request.svelte';
   import Response from './components/Response.svelte';
 </script>
-
-<svelte:head>
-  {@html github}
-</svelte:head>
 
 <div class="container mx-auto">
   <Request />
