2046194bca816c34d9f59c31b8ac478b83c3784b
SirReiva <gjjavicoru@hotmail.com>
2020-07-29 14:16:06 +0200

    feat: added JSON Viewer  component

---
+++ src/components/JsonViewer.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import JSONFormatter from 'json-formatter-js';
+
+  export let code: string;
+  let el: HTMLElement;
+
+  $: {
+    if (el) {
+      const formatter = new JSONFormatter(code, 2, {
+        hoverPreviewEnabled: true,
+        hoverPreviewArrayCount: 100,
+        hoverPreviewFieldCount: 5,
+        theme: '',
+        animateOpen: true,
+        animateClose: true,
+        useToJSON: true
+      });
+      el.appendChild(formatter.render());
+    }
+  }
+</script>
+
+<style>
+
+</style>
+
+<div bind:this={el} />
