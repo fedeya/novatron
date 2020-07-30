e2ce6dc7ace0941f20538ca96e41b3b872d8f57e
SirReiva <gjjavicoru@hotmail.com>
2020-07-31 00:33:17 +0200

    feat: custom appbar

---
+++ src/components/Appbar.svelte
@@ -0,0 +1,87 @@
+<script>
+  import { onMount } from 'svelte';
+  const remote = require('electron').remote;
+  const currentWindow = remote.getCurrentWindow();
+
+  let maximized = currentWindow && currentWindow.isMaximized();
+
+  function onChangeState() {
+    console.log('change', currentWindow.isMaximized());
+    maximized = currentWindow && currentWindow.isMaximized();
+  }
+
+  onMount(() => {
+    currentWindow.addListener('maximize', onChangeState);
+    currentWindow.addListener('unmaximize', onChangeState);
+    currentWindow.addListener('restore', onChangeState);
+    return () => {
+      currentWindow && currentWindow.removeAllListeners();
+    };
+  });
+  function close() {
+    currentWindow && currentWindow.close();
+  }
+  function minimize() {
+    currentWindow && currentWindow.minimize();
+  }
+  function restore() {
+    currentWindow && currentWindow.restore();
+  }
+  function maximize() {
+    currentWindow && currentWindow.maximize();
+  }
+</script>
+
+<style>
+  .appbar {
+    height: 42px;
+    width: 100vw;
+  }
+  .iconTitle {
+    -webkit-user-select: none;
+    -webkit-app-region: drag;
+  }
+  .btnAction {
+    cursor: default;
+    height: 100%;
+    width: 42px;
+    display: inline-flex;
+  }
+</style>
+
+<div class="appbar flex bg-primary-500 text-white">
+  <div class="iconTitle flex flex-1 items-center pl-4">NoVaTRoN</div>
+  <div class="flex-initial">
+    <span
+      class="btnAction material-icons hover:bg-primary-800 justify-center
+      items-center"
+      on:click={minimize}
+    >
+      minimize
+    </span>
+    {#if maximized}
+      <span
+        class="btnAction material-icons hover:bg-primary-800 justify-center
+        items-center"
+        on:click={restore}
+      >
+        content_copy
+      </span>
+    {:else}
+      <span
+        class="btnAction material-icons hover:bg-primary-800 justify-center
+        items-center"
+        on:click={maximize}
+      >
+        crop_square
+      </span>
+    {/if}
+    <span
+      class="btnAction material-icons hover:bg-error-800 justify-center
+      items-center"
+      on:click={close}
+    >
+      close
+    </span>
+  </div>
+</div>
